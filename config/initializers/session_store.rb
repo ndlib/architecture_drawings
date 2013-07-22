85e77db67a40fbf299e436f673082dc3fd64abda
Jaron Kennel <jaronkk@gmail.com>
2013-07-21 22:00:29 -0400

    New Rails 3.2 app

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ArchitectureBlueprints::Application.config.session_store :cookie_store, key: '_architecture_blueprints_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ArchitectureBlueprints::Application.config.session_store :active_record_store
