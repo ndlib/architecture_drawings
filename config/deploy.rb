7162bd46eeda7bf38fdb519a6620edb0b894ac48
Jaron Kennel <jaronkk@gmail.com>
2013-08-05 15:00:21 -0400

    Attempting full deploy to pprd

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,13 +23,11 @@ set :application, 'architecture_drawings'
 desc "Setup for the Pre-Production environment"
 task :pre_production do
   # Customize pre_production configuration
-  set :domain,    "rpprd.library.nd.edu"
 end
 
 desc "Setup for the production environment"
 task :production do
   # Customize production configuration
-  set :domain,    "rprod.library.nd.edu"
 end
 
 #############################################################
